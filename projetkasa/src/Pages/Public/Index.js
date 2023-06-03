22864fa7fa949f65f822e16bba07b64f80ca6057
4Sitam4 <109106334+4Sitam4@users.noreply.github.com>
2023-06-03 13:52:48 +0200

    Init React Project

---
+++ projetkasa/src/Pages/Public/Index.js
@@ -0,0 +1,4 @@
+// export des pages pour appel général dans le router
+export { default as Home } from './Home/Home.js'
+export { default as Apropos } from './Apropos/Apropos.js'
+export { default as Logement } from './Logement/Logement.js'
