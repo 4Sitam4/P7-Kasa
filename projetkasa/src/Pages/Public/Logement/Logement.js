894055a6ee0d68f144fc0fb43205ef79a5b84c11
4Sitam4 <109106334+4Sitam4@users.noreply.github.com>
2023-06-30 16:10:54 +0200

    Create component Carrousel (still building)

--- projetkasa/src/Pages/Public/Logement/Logement.js
+++ projetkasa/src/Pages/Public/Logement/Logement.js
@@ -1,13 +1,21 @@
 // Description: Page d'accueil du site
 
 import '@/Pages/Public/Logement/Logement.css';
-import useLogementService from '@/_Services/Logement.service.js';
+import useLogementService from '@/_Services/LogementByID.service.js';
+// import Carrousel from '@/Components/Carrousel/Carrousel.js';
 
 // fonction d'appel de la page d'accueil
 const Logement = () => {
-    const logementService = useLogementService();
+    const GetLogementById = useLogementService();
+
+    // récupération de l'id du logement
+    let url = new URL(window.location.href);
+    let sections = url.pathname.split('/');
+    let id = sections[sections.length - 2];
+
     return (
         <section className="logement">
+            
             
         </section>
     );
